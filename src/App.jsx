76d33d7144bd5d597103d0ef16afc25d440974ef
selvakumar <selvakumarv357@gmail.com>
2026-01-02 11:57:03 +0530

    client

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import AddUser from './addUser/AddUser';
+import './App.css'
+import User from './getUser/User'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+function App() {
+  const route = createBrowserRouter([
+    {
+      path:'/',
+      element: <User />
+    },
+    {
+      path: '/add',
+      element: <AddUser />
+    }
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={route}></RouterProvider>
+    </div>
+  )
+}
+
+export default App
---
+++ src/getUser/User.jsx
@@ -0,0 +1,63 @@
+import React, { useState,useEffect } from 'react';
+import "./user.css";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const User = () => {
+const [users,setUsers] = useState([]);
+
+useEffect(()=>{
+    const fetchData = async() =>{
+        try {
+            const response = await axios.get("http://localhost:5000/api/users");
+            setUsers(response.data)
+        } catch (error) {
+            console.log("Error while fetching data", error)
+        }
+    };
+    fetchData()
+},[]);
+ 
+  return (
+    <div className="userTable">
+        <Link to='/add' type="button" class="btn btn-primary">
+            Add User <i class="fa-solid fa-user-plus"></i>
+        </Link>
+        <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th scope= "col">S.No</th>
+                    <th scope= "col">Name</th>
+                    <th scope= "col">Email</th>
+                    <th scope= "col">Address</th>
+                    <th scope= "col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user, index)=>{
+                    return(
+                    <tr>
+                    <td>{index+1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.address}</td>
+                    <td className='actionButtons'>
+                        <button type="button" class="btn btn-success">
+                            <i class="fa-regular fa-pen-to-square"></i>
+                        </button>
+                        <button type="button" class="btn btn-danger">
+                            <i class="fa-solid fa-delete-left"></i>
+                        </button>
+                    </td>
+                </tr>
+                    )
+                })}
+
+            </tbody>
+        </table>
+    
+    </div>
+  )
+}
+
+export default User;
